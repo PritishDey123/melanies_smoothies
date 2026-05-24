adbe2297a30dbe1867d1f198573275af9cf4bace
PritishDey123 <pritish.dey@triedatum.com>
2026-05-24 16:09:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,10 +5,11 @@ from snowflake.snowpark.functions import col
 import requests
 import json
 
-# Write directly to the app
+# App title
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write("Choose the fruits you want in your custom Smoothie!")
 
+# Input
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
@@ -16,7 +17,7 @@ st.write('The name on your Smoothie will be:', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-# Get both columns
+# Get data
 my_dataframe = session.table("smoothies.public.fruit_options") \
     .select(col('FRUIT_NAME'), col('SEARCH_ON'))
 
@@ -37,30 +38,27 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
-        # Get SEARCH_ON value
         search_on = pd_df.loc[
             pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'
         ].iloc[0]
 
-        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+        st.write('The search value for ', fruit_chosen, ' is ', search_on)
 
         st.subheader(fruit_chosen + ' Nutrition Information')
 
-        # API call using SEARCH_ON
-        smoothiefroot_response = requests.get(
-            "https://my.smoothiefroot.com/api/fruit/" + search_on
+        response = requests.get(
+            "https://my.smoothiefroot.com/api/fruit/" + search_on.lower()
         )
 
-        # Convert JSON to table format
-        sf_df = st.dataframe(
-            pd.json_normalize(json.loads(smoothiefroot_response.text)),
-            use_container_width=True
-        )
+        data = json.loads(response.text)
+        df = pd.json_normalize(data)
 
-    # Insert query
-    my_insert_stmt = """ 
+        st.dataframe(df, use_container_width=True)
+
+    # Insert into Snowflake
+    my_insert_stmt = f"""
         INSERT INTO smoothies.public.orders(ingredients, name_on_order)
-        VALUES ('""" + ingredients_string + """','""" + name_on_order + """')
+        VALUES ('{ingredients_string}', '{name_on_order}')
     """
 
     if st.button('Submit Order'):
